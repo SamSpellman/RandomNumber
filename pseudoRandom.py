3afa33fcee410e20a9f0ff1caa85e63eab6b065d
SamSpellman <sspellman2@wisc.edu>
2022-08-10 22:33:39 -0500

    Added comments for lcg

---
+++ pseudoRandom.py
@@ -0,0 +1,10 @@
+# The first approach I will try is just the linear congruential generator.
+
+def linCongrGen():
+    # 
+
+# This function will initialize the generator by calculating ten values
+
+# This is the calculation for the next number.
+#
+# param num is the last number that was calculated (or the first term)
