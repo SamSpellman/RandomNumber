eb6e0377237a2014dd051c025fda4268aac53a69
SamSpellman <sspellman2@wisc.edu>
2022-08-11 23:40:11 -0500

    Defined methods to use, researched coefficients for this model

--- pseudoRandom.py
+++ pseudoRandom.py
@@ -1,10 +1,20 @@
 # The first approach I will try is just the linear congruential generator.
+# The coefficients are very important to get a statistically random 
+# distribution, but I will not concern myself with this at the current moment.
+# Function used:        X[n+1] = (aX[n] + c) % m
 
 def linCongrGen():
     # 
+    return 0
 
 # This function will initialize the generator by calculating ten values
+def seedAndRecur():
+    # this will seed the generator and iterate to get it started
+    return 0
 
 # This is the calculation for the next number.
 #
 # param num is the last number that was calculated (or the first term)
+def nextNumCongruence(lastNum):
+    # the calculation is parameterized by the previous number
+    return 0
